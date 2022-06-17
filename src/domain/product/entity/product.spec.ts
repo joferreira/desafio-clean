f5e8444ed2dbe4bff1256a177429fcb756ff8ef6
joferreira <jf.sorin@gmail.com>
2022-06-17 19:25:30 -0300

    Desafio Notification Pattern em Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,19 +5,25 @@ describe("Product unit tests", () => {
     it("should throw error when id is empty", () => {
         expect(()=> {
             const product = new Product("", "Product 1", 100);
-        }).toThrowError("Id is required");
+        }).toThrowError("product: Id is required");
     });
 
     it("should throw error when name is empty", () => {
         expect(()=> {
             const product = new Product("p1", "", 100);
-        }).toThrowError("Name is required");
+        }).toThrowError("product: Name is required");
     });
 
     it("should throw error when price is less than 0", () => {
         expect(()=> {
             const product = new Product("p1", "Product 1", -100);
-        }).toThrowError("Price must be greater than 0");
+        }).toThrowError("product: Price must be greater than 0");
+    });
+
+    it("should throw error when name and id are empty and price is less than 0", () => {
+        expect(()=> {
+            const product = new Product("", "", -100);
+        }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than 0");
     });
 
     it("should change name", () => {
